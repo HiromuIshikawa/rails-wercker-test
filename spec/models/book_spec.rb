7e14fbd46715e77022644e9cb0adb416c304017a
Hiromu Ishikawa <rock.lee.012086@gmail.com>
2017-07-05 21:33:40 +0900

    Add test

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -3,3 +3,11 @@ require 'rails_helper'
 RSpec.describe Book, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 end
+
+RSpec.describe Book, :type => :model do
+  it "isn't valid without title" do
+    book = Book.new
+    book.title = nil
+    expect(book).not_to be_valid
+  end
+end
